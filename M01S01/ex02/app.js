f13e88edd91259f03291976688aef4411c3f8f25
ianiri <44301798+ianiri@users.noreply.github.com>
2020-05-19 22:09:19 +0300

    M01 S01 & juma de M01 S02

--- M01S01/ex02/app.js
+++ M01S01/ex02/app.js
@@ -1,1 +1,18 @@
 
+var inputLenght = document.getElementById('length');
+var inputWidth = document.getElementById('width');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  var length = inputLenght.value || 0;
+  var width = inputWidth.value || 0;
+  var result = 0;
+
+  result = length * width;
+
+  elementResult.innerText = result;
+
+  e.preventDefault()
+}, false);
+
