40b52eeaf056bba554ef9cd98eeacfbd83155acd
ianiri <44301798+ianiri@users.noreply.github.com>
2020-06-29 19:39:37 +0300

    Tema 25 iunie

---
+++ hangman/js/app.js
@@ -0,0 +1,150 @@
+let words = ['spinach', 'tomato', 'crocodile', 'keyboard'];
+let tries = 5;
+let word = words[Math.floor(Math.random() * words.length)];
+let guesedLetters = [];
+let gameEnded = false;
+let wordGuesed = false;
+let keyboard = document.querySelector('.keyboard');
+let resetButton = document.querySelector('.button-reset');
+let message = document.querySelector('.message');
+
+let renderTries = () => {
+  let triesElement = document.getElementById('js-tries');
+
+  triesElement.innerText = tries;
+};
+
+let renderWord = () => {
+  let wordContainerElement = document.getElementById('js-word');
+  let wordLength = word.length;
+  let okIterationCount = 0;
+  wordContainerElement.innerHTML = '';
+
+  word.split('').forEach((letter) => {
+    let button = document.createElement('button');
+    
+    button.classList.add('btn', 'btn-info', 'm-1');
+    
+    if (guesedLetters.includes(letter)) {
+      okIterationCount++;
+      button.innerText = letter;
+    }
+
+    wordContainerElement.appendChild(button);
+  });
+
+  if (okIterationCount === wordLength) {
+    wordGuesed = true;
+  } else {
+    wordGuesed = false;
+  }
+};
+
+let checkGameState = () => {
+  let keyboardButtons = keyboard.querySelectorAll('.btn-secondary');
+  if (tries === 0) {
+    gameEnded = true;
+    keyboardButtons.forEach((button) => {
+      button.disabled = true;
+    });  
+  } else {
+    gameEnded = false;
+  }
+
+  if (gameEnded && wordGuesed) {
+    message.innerText = 'Yaaaay, ai castigat!';
+    keyboardButtons.forEach((button) => {
+      button.disabled = true;
+    });  
+    return;
+  }
+
+  if (wordGuesed) {
+    message.innerText = 'Yaaaay, ai castigat!';
+    keyboardButtons.forEach((button) => {
+      button.disabled = true;
+    });  
+    return;
+  }
+
+  if (gameEnded) {
+    message.innerText = 'Uff, ai pierdut!';
+    keyboardButtons.forEach((button) => {
+      button.disabled = true;
+    });  
+    return;
+  }
+};
+
+let renderFace = () => {
+  let face = document.getElementById('js-face');
+  let eyes = face.querySelectorAll('.eye');
+  let classNames = '';
+
+  eyes.forEach((eye) => {
+    eye.classList.remove('eye--worried', 'eye--annoyed');
+  })
+
+  if (tries >= 2 && tries <= 3) {
+    classNames += 'eye--annoyed';
+  }
+  
+  if (tries === 1) {
+    classNames += 'eye--worried';
+  }
+
+  if (classNames.length < 1) {
+    return;
+  }
+
+  eyes.forEach((eye) => {
+    eye.classList.add(classNames);
+  });
+};
+
+renderTries();
+renderWord();
+renderFace();
+
+keyboard.addEventListener('click', (event) => {
+  if (event.target && event.target.nodeName === 'BUTTON') {
+    let letter = event.target.innerText.toLowerCase();
+
+    guesedLetters.push(letter);
+    event.target.disabled = true;
+
+    if (word.includes(letter)) {
+      event.target.classList.add('btn-success');
+    } else {
+      tries--;
+      event.target.classList.add('btn-danger');
+    }
+
+    renderTries();
+    renderWord();
+    checkGameState();
+    renderFace();
+  }
+});
+
+  resetButton.addEventListener('click', () => {
+    tries = 5;
+    word = words[Math.floor(Math.random() * words.length)];
+    guesedLetters.length = 0;
+    let okIterationCount = 0;
+    let buttons = document.querySelectorAll('[disabled]');
+
+    buttons.forEach((button) => {
+      button.classList.remove('btn-danger');
+      button.classList.remove('btn-success');
+      button.disabled = false;
+    });
+
+    message.innerText = 'Ok, hai sa incercam cu un cuvant nou.';
+    setTimeout (() => {
+      message.innerText = '';
+    }, 6000);
+
+    renderTries();
+    renderWord();
+  });
