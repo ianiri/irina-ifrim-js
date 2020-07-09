7d9597acfbe3de77b44799785528d29dfb8e805d
ianiri <44301798+ianiri@users.noreply.github.com>
2020-07-09 22:20:12 +0300

    M02S02 - jQuery/ part II

--- M02S02/ex02/app.js
+++ M02S02/ex02/app.js
@@ -7,15 +7,39 @@ $(document).ready(() => {
   let $form = $('#personForm');
   let $personDetails = $('.person-details');
 
+  let textCaptureForm = () => {
+    let $formFragment = $('<div>');
+
+    return $formFragment
+                .append('<label for="skill-rename">Edit</label>')
+                .append($('<input>', {
+                  type: 'text',
+                  name: 'skill-rename',
+                })
+              )
+                .append(`
+                  <button type="button" class="skill-edit-save">Salvare</button>
+                  <button type="button" class="skill-edit-cancel">Anulare</button>
+                  `);
+  };
+
   $form.on('submit', (event) => {
     let $formFields = $(event.currentTarget).serializeArray();
     let $personFragment = $('<div>');
 
     $personDetails.empty();
 
+    // $formFields.forEach((field) => {
+    //   $personFragment.append($('<p>', {
+    //     text: `${field.name[0].toUpperCase()}${field.name.slice(1)}: ${field.value}`,
+    //   }));
+    // });
+
     $formFields.forEach((field) => {
+      let $labelElement = $(`input[name="${field.name}"]`).prev();
+      let displayName = $labelElement.text();
       $personFragment.append($('<p>', {
-        text: `${field.name[0].toUpperCase()}${field.name.slice(1)}: ${field.value}`,
+        text: `${displayName} ${field.value}`,
       }));
     });
 
@@ -25,4 +49,89 @@ $(document).ready(() => {
     event.currentTarget.reset();
     event.preventDefault();
   });
+
+  // In fieldsetul Detalii adauga un input de abilitati numit skills si un buton de + in dreptul sau. La fiecare apasare a butonului, daca in input exista text, afiseaza abilitatea noua intr-o lista neordonata.
+  // In dreptul fiecarui element din lista adauga un buton de stergere. In momentul in care se apasa sterge elementul.
+  //   In dreptul fiecarei abilitati din lista neordonata adauga si un buton de editare. In momentul in care este apasat, inlocuieste textul cu un input si butoane de salvare si anulare. La salvare, schimba textul abilitatii existente.
+  // Adauga un checkbox “Are animale de casa” nebifat. In momentul in care acesta este apasat, afiseaza un nou fieldset cu campurile Rasa, Nume si Varsta si un buton de Salvare. Cand butonul este apasat, afiseaza in a treia lista neordonata datele fiecarui animal si goleste acele campuri.
+  // Cand checkboxul este debifat, elimina fieldsetul.
+
+
+  $('#addSkill').on('click', (event) => {
+    // let skillName = $(event.currentTarget).prev().val();
+    let $skillInput = $(event.currentTarget).prev();
+    let skillName = $skillInput.val();
+
+    if (skillName.length < 1) {
+      return;
+    }
+
+    let ulClass = 'skills-ul';
+    let $skillsUl = $(`.${ulClass}`);
+
+    if ($skillsUl.length < 1) {
+      $skillsUl = $('<ul>', {
+        class: ulClass,
+      }).appendTo('.person-skills')
+        .on('click', '.skill-delete, .skill-edit-cancel', (event) => {
+        // if (event.target && event.target.nodeName === 'BUTTON' )
+        $(event.currentTarget).parent().remove();
+        })
+        .on('click', '.skill-edit', (event) => {
+          $(event.currentTarget).parent().prepend(textCaptureForm());
+        })
+        .on('click', '.skill-edit-save', (event) => {
+          let $skillEditSave = $(event.currentTarget);
+          let newSkillName = $skillEditSave.prev().val();
+
+          if (newSkillName.length < 1) {
+            return;
+          }
+
+          $skillEditSave.parents('li').find('span').text(newSkillName);
+          $skillEditSave.parent().remove();
+        });
+    }
+
+
+
+    $skillsUl.append(`<li>
+                      <span>${skillName}</span>
+                      <button class="skill-delete">Sterge</button>
+                      <button class="skill-edit">Editeaza</button>
+                      </li>`);
+
+    $skillInput.val('');
+  });
+
+  $('#has-pets').on('change', (event) => {
+    let $checkBox = $(event.currentTarget);
+    let isChecked = $checkBox.is(':checked');
+    let $petForm = $checkBox.siblings('.pet-form');
+
+    if (isChecked) {
+      $petForm.show();
+    } else {
+      $petForm.hide();
+    }
+  });
+
+  $('#addPet').on('click', (event) => {
+    let $addPetButton = $(event.currentTarget);
+    let $petForm = $addPetButton.parent();
+
+    let $fields = $petForm.children('input[name]');
+    let formFields = [];
+    $fields.each((index, domElement) => {
+      let $field = $(domElement);
+    
+      formFields.push({
+        name: $field.attr('name'),
+        value: $field.val(),
+      });
+    });
+
+    console.log(formFields);
+  });
 });
+
